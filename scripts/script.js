99bf45f65c7e8befd55ced5e817827736b5884a9
Fernando Bertino <155702767+fernando-bertino@users.noreply.github.com>
2024-07-27 03:07:04 -0300

    Add files via upload
    Correção do menu no modo mobile.

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,44 @@
 const menu = document.querySelector('#menu-icon')
 const navMenu = document.querySelector('.nav-menu')
-const smain = document.querySelector('main-section')
+const smain = document.querySelector('.main-section')
+const foot = document.querySelector('footer')
 
-menu.addEventListener('click', function() {
+menu.addEventListener('click', function () {
     // Funcionalidades do botão menu
     menu.classList.toggle('ativo')
-    if (menu.classList == 'material-icons ativo'){
+    if (menu.classList == 'material-icons ativo') {
         menu.innerHTML = 'close'
         menu.style.transition = 'all .5s'
         menu.style.color = 'var(--color1)'
         menu.style.transform = 'rotate(180deg)'
+        smain.style.display = 'none'
+        foot.style.display = 'none'
+
     } else {
         menu.innerHTML = 'menu'
         menu.style.transition = 'all .5s'
         menu.style.color = 'white'
         menu.style.transform = 'rotate(0)'
+        smain.style.display = 'block'
     }
 
     // Funcionalidade da tag nav
     navMenu.classList.toggle('ativo')
 })
 
-function escrevendoLetra(){
-    function ativaLetra(elemento){
+function escrevendoLetra() {
+    function ativaLetra(elemento) {
         const arrTexto = elemento.innerHTML.split('')
         elemento.innerHTML = ''
-        arrTexto.forEach((letra, i)=>{
-            setTimeout(()=>{
+        arrTexto.forEach((letra, i) => {
+            setTimeout(() => {
                 elemento.innerHTML += letra
             }, 75 * i)
         })
     }
-    
+
     const titulo = document.querySelector('.typewriter')
-    
+
     ativaLetra(titulo)
 }
 
